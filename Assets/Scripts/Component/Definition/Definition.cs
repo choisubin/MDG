bb33c6af6db8c1b208827d82eccffc264125fe8d
chsbin99 <chsbin99@gmail.com>
2022-04-28 16:53:16 +0900

    +add Unity file
    유니티 파일 추가
    repository 옮김
    gitignore적용

---
+++ Assets/Scripts/Component/Definition/Definition.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Definition : MonoBehaviour
+{
+
+}
+
+[Serializable]
+public class StageDefinition
+{
+    public int key;
+    public StageDetailDefinition[] detailStage;
+}
+
+[Serializable]
+public class StageDetailDefinition
+{
+    public int key;
+    public int stageNum;
+    public int semiStageNum;
+    public int stageDetailMapKey;
+}
+
+[Serializable]
+public class StageDetailMapDefinition
+{
+    public int key;
+    public int width;
+    public int height;
+    public int[][] mapGrid;
+}
+
+[Serializable]
+public class StageDetailMapDefinitionContainer : ILoader<int, StageDetailMapDefinition>
+{
+    public List<StageDetailMapDefinition> definitions = new List<StageDetailMapDefinition>();
+    public Dictionary<int, StageDetailMapDefinition> MakeDict()
+    {
+        Dictionary<int, StageDetailMapDefinition> dict = new Dictionary<int, StageDetailMapDefinition>();
+        foreach (StageDetailMapDefinition definition in definitions)
+        {
+            dict.Add(definition.key, definition);
+        }
+        return dict;
+    }
+}
---
+++ Assets/Scripts/Game/UI/tempBtn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempBtn : MonoBehaviour
+{
+    List<GameObject> list = new List<GameObject>();
+    public void OnClick()
+    {
+        Hashtable sendData = new Hashtable();
+        sendData.Add(EDataParamKey.Integer, EGameState.INGAME);
+        NotificationCenter.Instance.PostNotification(Notification.Instantiate(ENotiMessage.ChangeSceneState, sendData));
+    }
+
+    public void OnClick2()
+    {
+        Hashtable sendData = new Hashtable();
+        sendData.Add(EDataParamKey.Integer, EGameState.LOBBY);
+        NotificationCenter.Instance.PostNotification(Notification.Instantiate(ENotiMessage.ChangeSceneState, sendData));
+    }
+    public void OnClick3()
+    {
+        GameObject go = PoolManager.Instance.GrabPrefabs(EPrefabsType.GameStateHandler, "test", this.transform);
+        list.Add(go);
+    }
+
+    public void OnClick4()
+    { 
+        PoolManager.Instance.DespawnObject(EPrefabsType.GameStateHandler, list[0]);
+        list.Remove(list[0]);
+    }
+}
