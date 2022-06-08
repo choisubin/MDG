8be87cf815a9842c7806d9020263a6ee6a10c9c8
choisubin <chsbin99@gmail.com>
2022-06-08 19:07:30 +0900

    +Stage Unit Scheduling

--- Assets/Scripts/Game/InGame/Common/AMVC/DefenseMapController.cs
+++ Assets/Scripts/Game/InGame/Common/AMVC/DefenseMapController.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class DefenseMapController : BaseController
 {
     [SerializeField]private DefenseMapBase _defenseMap;
+
     private List<UnitBase> _unitList = new List<UnitBase>();
+
     private Dictionary<int, UnitDefinition> _unitDefDic = new Dictionary<int, UnitDefinition>();
+
+    private Dictionary<float, List<int>> _currentScheduleDic = new Dictionary<float, List<int>>();
+
     public override void Init()
     {
         _unitDefDic = DefinitionManager.Instance.GetDatas<UnitDefinition>();
@@ -14,25 +19,19 @@ public class DefenseMapController : BaseController
 
     public override void Set()
     {
-        List<StageEnemySpawnDefinition> spawndef = DefinitionManager.Instance.GetData<List<StageEnemySpawnDefinition>>(1);
-        foreach(var s in spawndef)
-        {
-            Debug.LogError(string.Format("{0} {1} {2} {3}", s.SpawnUnitKey, s.SpawnUnitNum, s.SpawnCycle, s.SpawnUnitNum));
-        }
+        SetSpawnSchedule(1);
     }
 
-    float spawnTime = 0;
+    float _currentTime = 0;
     public override void AdvanceTime(float dt_sec)
     {
-        spawnTime += dt_sec;
+        _currentTime += dt_sec;
 
         RemoveEnemyToMap();
 
-        if (spawnTime > 0.1f)
-        {
-            SpawnEnemy(Random.Range(1, 9));
-            spawnTime = 0;
-        }
+        SpawnScheduling(_currentTime);
+
+        AdvanceUnits(dt_sec);
     }
 
     public override void Dispose()
@@ -40,6 +39,56 @@ public class DefenseMapController : BaseController
     }
 
     #region private Method
+
+    private void AdvanceUnits(float dt_sec)
+    {
+        foreach(var unit in _unitList)
+        {
+            unit.AdvanceTime(dt_sec);
+        }
+    }
+
+    private List<StageEnemySpawnDefinition> _spawnDefList = new List<StageEnemySpawnDefinition>();
+    private void SetSpawnSchedule(int defKey)
+    {
+        _spawnDefList = DefinitionManager.Instance.GetData<List<StageEnemySpawnDefinition>>(defKey);
+        foreach (var s in _spawnDefList)
+        {
+            float spawnTime = 0;
+            for(int i = 0; i < s.SpawnUnitNum;i++)
+            {
+                spawnTime = s.Time + (s.SpawnCycle * i);
+                if(!_currentScheduleDic.ContainsKey(spawnTime))
+                {
+                    _currentScheduleDic.Add(spawnTime, new List<int>());
+                }
+                _currentScheduleDic[spawnTime].Add(s.SpawnUnitKey);
+            }
+        }
+    }
+
+    private List<float> _scheduleRemoveList = new List<float>();
+    private void SpawnScheduling(float _currentTime)
+    {
+        _scheduleRemoveList.Clear();
+        foreach (var schedule in _currentScheduleDic)
+        {
+            if(_currentTime > schedule.Key)
+            {
+                _scheduleRemoveList.Add(schedule.Key);
+                foreach(var unitKey in schedule.Value)
+                {
+                    SpawnEnemy(unitKey);
+                }
+            }
+        }
+
+        foreach (var remove in _scheduleRemoveList)
+        {
+            _currentScheduleDic.Remove(remove);
+        }
+    }
+
     private UnitDefinition _unitDef;
     private GameObject _tempUnitObj;
     private UnitBase _tempUnitBase;
@@ -52,21 +101,22 @@ public class DefenseMapController : BaseController
         _unitList.Add(_tempUnitBase);
     }
 
-    private List<UnitBase> _removeList = new List<UnitBase>();
+    private List<UnitBase> _unitBaseRemoveList = new List<UnitBase>();
     private void RemoveEnemyToMap()
     {
-        _removeList.Clear();
+        _unitBaseRemoveList.Clear();
         foreach (var unit in _unitList)
         {
             if (!unit.IsAlive)
             {
-                _removeList.Add(unit);
+                _unitBaseRemoveList.Add(unit);
             }
         }
-        foreach (var remove in _removeList)
+        foreach (var remove in _unitBaseRemoveList)
         {
             _unitList.Remove(remove);
         }
     }
+
     #endregion
 }
